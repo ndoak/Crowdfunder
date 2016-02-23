f1dc3ec0ea2c164820f9b6d556db6cb7f27c0673
ndoak <natalieannedoak@gmail.com>
2016-02-23 18:15:48 -0500

    Move rewards form to partial and render in projects/new

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
-  has_many :rewards 
+  has_many :rewards
+  accepts_nested_attributes_for :rewards, reject_if: :all_blank
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,9 @@ Rails.application.routes.draw do
 
   get 'users/edit'
 
-resources :projects 
+resources :projects
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
