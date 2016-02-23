a4187030d95b3167204d4a2d5ff3903b422be312
ndoak <natalieannedoak@gmail.com>
2016-02-23 17:25:25 -0500

    Add nested reward form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -30,7 +30,7 @@ class ProjectsController < ApplicationController
 private
   def project_params
     params.require(:project)
-          .permit(:title, :description, :funding_goal, :start_date, :end_date)
-          # rewards_attributes[:title,:description,:amount])
+          .permit(:title, :description, :funding_goal, :start_date, :end_date,
+          rewards_attributes:[:title,:description,:amount])
   end
 end
