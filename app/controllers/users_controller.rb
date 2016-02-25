79dfff80368abde6c2e88a346e2c9f8edcfa0fcd
mwraich <mwraich28@gmail.com>
2016-02-25 17:45:07 -0500

    Link to profile page, and display all projects user has created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @owned_projects = @user.owned_projects
+    # @backed_projects =  @user.backed_projects
   end
 
   def new
