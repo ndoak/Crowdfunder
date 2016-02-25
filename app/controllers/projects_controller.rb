5bf604f7a8bcdfa84889cd6832fee630c7f15d4d
ndoak <natalieannedoak@gmail.com>
2016-02-25 17:15:30 -0500

    Working time remaining

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,8 +13,8 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
+    @project.owner = current_user
     authorize! :manage, @project
-
     if @project.save
       redirect_to projects_path, notice: "Project Saved!"
     else
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160225205743) do
-
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
